ac954481c16ea5473adeb07a5e51af80eadbbb9e
Joe Grant <joegrant93@hotmail.com>
2020-11-18 09:19:20 +0000

    Add project files.

---
+++ MoonPig/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MoonPig
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
